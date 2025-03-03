73f7f93b01128b36dd93c963d991758f357884c3
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-03-03 08:24:08 +0530

    Add files via upload

--- A3SCQ2.php
+++ A3SCQ2.php
@@ -1,4 +1,37 @@
 <?php
+
+function sortArrayAscending($array)
+{
+    asort($array);
+    return $array;
+}
+
+function sortArrayDescending($array)
+{
+    arsort($array);
+    return  $array;
+}
+function sortByKeysAcsending($array)
+{
+    ksort($array);
+    return $array;
+}
+function sortByKeysDescending($array)
+{
+    krsort($array);
+    return $array;
+}
+function my_sort($value)
+{
+    return $value %2!==0;
+}
+$array=[
+    "a"=>2,
+    "b"=>5,
+    "c"=>9,
+    "d"=>4,
+    "e"=>3,
+];
 do{
 echo"\n Menu: \n";
 echo "1. Sort the array by keys accending \n";
@@ -8,13 +41,42 @@ echo "4. Sort the array by values descending \n";
 echo "5. Filter the odd elements from the array \n";
 echo "6: Exit";
 echo "Enter your choice:";
-$choice=intval(trim(fgets(STDIN)));
+$choice=readline("enter your choice:");
 
 switch($choice)
 {
 case 1:
     echo "sorted Array by keys:\n";
     print_r(sortByKeysAcsending($array));    
+    break;
+
+case 2:
+    echo "sorted array by keys:\n";
+    print_r(sortByKeysDescending($array));
+    break;
+
+case 3:
+    echo "sorted array in Ascending order by values\n";
+    print_r(sortArrayAscending($array));
+    break;
+
+case 4:
+    echo "sorted array in Descending order by values\n";
+    print_r(sortArrayDescending($array));
+    break;
+
+case 5:
+    echo "Oddd element from the array:\n";
+    print_r(array_filter($array,'my_sort'));
+    break;
+case 6:
+    echo "Exiting...\n";
+    break;
+    default:
+    echo "invalid choice. please try again.\n";
+
 }
 
-?>
+} while ($choice !=5);
+
+?> 
